1cfb52545dd34df7a8f7646b9e3bcdc8e0a5f424
fangchaoqun <fang.chaoqun@nissei-china.cn>
2022-01-28 15:15:33 +0800

    chore: update mysql Dockerfile

--- mysql/setup.sh
+++ mysql/setup.sh
@@ -12,12 +12,12 @@ echo $(service mysql status)
 
 # sleep 3
 echo '2.创建数据库...'
-mysql -uroot -p123456 </mysql/create_db.sql
+mysql -h127.0.0.1 -uroot -p123456 </mysql/create_db.sql
 sleep 3
 echo '2.创建数据库完毕...'
 
 echo '3.开始导入数据...'
-mysql -uroot -p123456 </mysql/initial_data.sql
+mysql -h127.0.0.1 -uroot -p123456 </mysql/initial_data.sql
 echo '3.导入数据完毕...'
 
 sleep 3
@@ -25,7 +25,7 @@ echo $(service mysql status)
 echo 'mysql容器启动完毕,且数据导入成功'
 
 echo '4.修改mysql权限...'
-mysql -uroot -p123456 </mysql/privileges.sql
+mysql -h127.0.0.1 -uroot -p123456 </mysql/privileges.sql
 sleep 3
 echo '4.权限修改完毕...'
 
