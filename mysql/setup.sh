b294d6de3f71a2f74f7234e9bffb75c7dcfefafc
fangchaoqun <fang.chaoqun@nissei-china.cn>
2022-01-28 11:47:07 +0800

    chore: update mysql setup

--- mysql/setup.sh
+++ mysql/setup.sh
@@ -4,26 +4,24 @@ set -e
 #查看mysql服务的状态，方便调试，这条语句可以删除
 echo $(service mysql status)
 
-echo '1.启动mysql...'
+# echo '1.启动mysql...'
 #启动mysql
 # service mysql stop
 # service mysql restart
-service mysql start
+# service mysql start
 
-sleep 3
-
-echo $(service mysql status)
+# sleep 3
 echo '2.创建数据库...'
-mysql </mysql/create_db.sql
+mysql -u root -p 123456 </mysql/create_db.sql
 sleep 3
 echo '2.创建数据库完毕...'
 
 echo '3.开始导入数据...'
-mysql </mysql/initial_data.sql
+mysql -u root -p 123456 </mysql/initial_data.sql
 echo '3.导入数据完毕...'
 
 echo '4.修改mysql权限...'
-mysql </mysql/privileges.sql
+mysql -u root -p 123456 </mysql/privileges.sql
 sleep 3
 echo '4.权限修改完毕...'
 
